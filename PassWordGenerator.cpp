53f0eff5bbfbb9beec92c22ea69712200f5ffe83
Vedant Zope <119584707+Vedantzope-08@users.noreply.github.com>
2024-06-03 12:57:32 +0530

    Update PassWordGenerator.cpp
    Added the code for saving the password into file.

--- PassWordGenerator.cpp
+++ PassWordGenerator.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <cstdlib>
 #include <ctime>
+#include <fstream>
 
 using namespace std;
 
@@ -119,8 +120,11 @@ int main()
                 system("clear");
             #endif
         }
-        else
+        else if(p==0)
         {
+        	remove("Pass.txt");      	
+        	ofstream my_file("Pass.txt");
+        	my_file<<pass;
             break;
         }
     } while (i < 2);
